a9126cf6e6fea4399ac74cc1346114295b49affd
Canty <idcanty.2016@mymail.jcsu.edu>
2018-02-07 13:58:27 -0500

    first commit

---
+++ Telephone2.java
@@ -0,0 +1,11 @@
+
+public class Telephone2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	Phone phone1 = new Phone ("Android", "Galaxy Note 8", 2017, 150);
+	
+	}
+
+}
